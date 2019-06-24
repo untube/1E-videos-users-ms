9e1e021666c0c7a25f7ee18a596b69f253d3118e
baangaritar <baangaritar@unal.edu.co>
2019-06-24 12:54:09 -0500

    name, nickname

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) 
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nickname])
   end
 end
