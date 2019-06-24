533fd9d9cb60edba2bfafc1892aaa1ed7aa41861
baangaritar <baangaritar@unal.edu.co>
2019-06-24 12:43:25 -0500

    Try with sanitizer

--- app/controllers/registrations_controller.rb
+++
@@ -1,5 +0,0 @@
-class RegistrationsController < ::DeviseTokenAuth::RegistrationsController
-    # Prevent session parameter from being passed
-    # Unpermitted parameter: session
-    wrap_parameters format: []
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :ldap
   mount_devise_token_auth_for 'User', at: 'auth',
     controllers: {
-      sessions: 'sessions',
-      registrations: 'registrations'
+      sessions: 'sessions'
     }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
